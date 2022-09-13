341b5352347ffd9e7edc65a2d84ca18fe28e92d5
AShamsan <92398524+AShamsan@users.noreply.github.com>
2022-09-13 13:25:17 -0400

    Delete Assignment directory

--- Assignment/WeakPasswordException.java
+++
@@ -1,9 +0,0 @@
-public class WeakPasswordException extends RuntimeException {
-
-  public WeakPasswordException() {
-    super("The password is OK but weak - it contains fewer than 10 characters");
-  }
-
-
-
-}
